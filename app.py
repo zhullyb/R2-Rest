d4fdbfdd25a33753be381bd44005b7ac94b6f8fe
zhullyb <zhullyb@outlook.com>
2024-10-01 16:11:11 +0800

    add 404 handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, abort
 import botocore
 
 from r2 import R2
@@ -10,12 +10,19 @@ r2 = R2(info)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET'])
 def index(path):
+    if path == '':
+        abort(404)
     try:
         r2_object = r2.get_object(path)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'NoSuchKey':
-            return send_file('assets/404.jpg', mimetype='image/jpg'), 404
+            abort(404)
     return send_file(r2_object.get('Body'), mimetype=r2_object.get('ContentType'), download_name=path)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_file('assets/404.jpg', mimetype='image/jpg'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
